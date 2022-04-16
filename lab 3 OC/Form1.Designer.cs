f9a7ce399066e72bd412376481c4826ea743fef1
ZeroPrin <71882815+ZeroPrin@users.noreply.github.com>
2022-04-16 07:10:55 +0500

    Добавьте файлы проекта.

---
+++ lab 3 OC/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace lab_3_OC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(62, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(423, 53);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(336, 349);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(323, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab 3 OC/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab_3_OC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int[] array = new int[10];
+        static Thread threadOne = new Thread(sort1);
+        static Thread threadTwo = new Thread(sort2);
+        static bool flag, flag1 = true;
+        static object locker1 = new object();
+        static object locker2 = new object();
+        static void sort1()
+        {
+            int per;
+            for (int i = 0; i < array.Length; ++i)
+            {
+                for (int j = i + 1; j < array.Length; ++j)
+                {
+                    if (array[i] < array[j])
+                    {
+                        per = array[i];
+                        array[i] = array[j];
+                        array[j] = per;
+                    }
+                }
+            }
+        }
+        static void sort2()
+        {
+            int per;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+                per = array[min];
+                array[min] = array[i];
+                array[i] = per;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = rand.Next(-100, 100);
+                richTextBox1.Text += array[i].ToString() + " ";
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            if (flag == true)
+            {
+                richTextBox1.Clear();
+                threadOne.Start();
+                threadTwo.Start();
+                for (int i = 0; i < array.Length; ++i)
+                    richTextBox1.Text += array[i].ToString() + " ";    
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; ++i)
+                    richTextBox1.Text += array[i].ToString() + " ";
+                sort1();
+                richTextBox1.Text += "\n";
+                for (int i = 0; i < array.Length; ++i)
+                    richTextBox1.Text += array[i].ToString() + " ";
+                sort2();
+                richTextBox1.Text += "\n";
+                for (int i = 0; i < array.Length; ++i)
+                    richTextBox1.Text += array[i].ToString() + " ";
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                flag = true;
+            else
+                flag = false;
+        }
+    }
+}
